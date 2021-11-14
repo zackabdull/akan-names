9bf97d0e47e806f4482fad76dd8d625423eff10d
zackabdull <zakaria.maalim@student.moringaschool.com>
2021-11-14 14:25:38 +0300

    add the variables for the day and names

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+var maleNames = ["Kwasi", "Kwado", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+
+var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+function result(){
+
+
+    var year = parseInt(document.getElementById("year").value);
+
+
+    var month = parseInt(document.getElementById("month").value);
+
+    var day = parseInt(document.getElementById("day").value);
+
+
+    var male = document.getElementById("male")
+
+    var female = document.getElementById("female")
+    
