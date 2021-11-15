6b10bfb60b7827d128094fda6c64d4f226848247
zackabdull <zakaria.maalim@student.moringaschool.com>
2021-11-15 06:20:51 +0300

    js not working so used the formular given the ip

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,21 @@
 function fn() {
-  var d = new Date();
-  var year = parseInt(document.getElementById("year").value);
+  var yy = parseInt(document.getElementById("year").value);
 
-  var month = parseInt(document.getElementById("month").value);
+  var mm = parseInt(document.getElementById("month").value);
 
-  var date = parseInt(document.getElementById("day").value);
-  //the validation
-  if (year <= 1800 || year > 2020) {
-    alert("Input the correct year!");
+  var dd = parseInt(document.getElementById("day").value);
+
+  var cc = parseInt(document.getElementById("century").value);
+  //validation
+  if (dd <= 0 || dd > 31) {
+    alert("Input the correct day!");
   } else if (month <= 0 || month > 12) {
     alert("Input correct month!");
-  } else if (date <= 0 || date > 31) {
-    alert("Input correct day!");
   }
-  var day = d.getDay();
+
+  var day = parseInt(
+    (cc / 4 - 2 * cc - 1 + (5 * yy) / 4 + (26 * (mm + 1)) / 10 + dd) % 7
+  );
   var male = ["Kwasi", "Kwado", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
   var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
@@ -27,44 +29,13 @@ function fn() {
     "Friday",
     "Saturday",
   ];
-  function getAkan() {
-    var isMale = document.getElementById("male").checked;
+  var isMale = document.getElementById("male").checked;
 
-    var isFemale = document.getElementById("female").checked;
-    if (isMale == true && isFemale == false) {
-      if (day == 0) {
-        alert(" and you Akan name is " + male[0]);
-      } else if (day == 1) {
-        alert(" and you Akan name is " + male[1]);
-      } else if (day == 2) {
-        alert(" and you Akan name is " + male[2]);
-      } else if (day == 3) {
-        alert(" and you Akan name is " + male[3]);
-      } else if (day == 4) {
-        alert(" and you Akan name is " + male[4]);
-      } else if (day == 5) {
-        alert(" and you Akan name is " + male[5]);
-      } else if (day == 6) {
-        alert(" and you Akan name is " + male[6]);
-      }
-    } else if (isMale == false && isFemale == true) {
-      if (day == 0) {
-        alert(" and you Akan name is " + female[0]);
-      } else if (day == 1) {
-        alert(" and you Akan name is " + female[1]);
-      } else if (day == 2) {
-        alert(" and you Akan name is " + female[2]);
-      } else if (day == 3) {
-        alert(" and you Akan name is " + female[3]);
-      } else if (day == 4) {
-        alert(" and you Akan name is " + female[4]);
-      } else if (day == 5) {
-        alert(" and you Akan name is " + female[5]);
-      } else if (day == 6) {
-        alert(" and you Akan name is " + female[6]);
-      }
-    }
+  var isFemale = document.getElementById("female").checked;
+  if (isMale == true && isFemale == false) {
+    alert(" and you Akan name is " + male[day]);
+  } else if (isMale == false && isFemale == true) {
+    alert(" and you Akan name is " + female[day]);
   }
-
-  getAkan();
 }
+fn();
